594906497a93b031e5e7aef8716af3a473f03d2f
ewand <ewan98davies@gmail.com>
2021-11-25 21:30:10 +0000

    updated image

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 app="docker.christmas.tree.controller"
 docker build -t ${app} .
-docker run -d -p 56733:80 \
+docker run --platform linux/arm/v7 -d -p 56733:80 \
   --name=${app} \
   -v $PWD:/app ${app}
