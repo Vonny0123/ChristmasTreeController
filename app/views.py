adeb4b795b0dbfafdcfba8adf38c0f214f8ea3f2
ewand <ewan98davies@gmail.com>
2021-11-27 16:58:35 +0000

    added buttons

--- app/views.py
+++ app/views.py
@@ -1,12 +1,18 @@
-from app import app
-from flask import render_template
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return "<b>There has been a change</b>"
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        if request.form.get("action1") == "VALUE1":
+            pass  # do something
+        elif request.form.get("action2") == "VALUE2":
+            pass  # do something else
+        else:
+            pass  # unknown
+    elif request.method == "GET":
+        return render_template("home.html", form=form)
 
-
-@app.route("/template")
-def template():
     return render_template("home.html")
