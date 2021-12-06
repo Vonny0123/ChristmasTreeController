1010fbe5cabb47a938509e196d69aff0071f3b49
ewand <ewan98davies@gmail.com>
2021-12-06 20:01:13 +0000

    switch to ajax

---
+++ app/static/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    $("#on_click").click(function () {
+        $.ajax({
+            type: "POST",
+            url: "/on_click",
+            success: function () {
+                alert("Tree On.");
+            }
+        });
+    });
+
+    $("#off_click").click(function () {
+        $.ajax({
+            type: "POST",
+            url: "/off_click",
+            success: function () {
+                alert("Tree Off.");
+            }
+        });
+    });
+
+    $("#cycle_click").click(function () {
+        $.ajax({
+            type: "POST",
+            url: "/cycle_click",
+            success: function () {
+                alert("Cycle on.");
+            }
+        });
+    });
+
+    $("#one_by_one_click").click(function () {
+        $.ajax({
+            type: "POST",
+            url: "/one_by_one_click",
+            success: function () {
+                alert("One by one on.");
+            }
+        });
+    });
+
+    $("#sparkle_click").click(function () {
+        $.ajax({
+            type: "POST",
+            url: "/sparkle_click",
+            success: function () {
+                alert("Sparkle On.");
+            }
+        });
+    });
+});
